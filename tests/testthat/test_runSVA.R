bb150059c11f1054c36494ce06cec1cfeb443f19
Mohammed Ali <mohammed@aggregate-genius.com>
2021-03-24 08:07:27 -0700

    fixed runSVA unit tests errors

--- tests/testthat/test_runSVA.R
+++ tests/testthat/test_runSVA.R
@@ -2,7 +2,7 @@ context("DGEobj.utils - tests for runSVA.R functions")
 
 
 test_that("runSVA.R: runSVA()", {
-    dgeObj_sva <- runSVA(dgeObj = t_obj1, designMatrixName = "ReplicateGroup")
+    dgeObj_sva <- runSVA(dgeObj = t_obj1, designMatrixName = "ReplicateGroupDesign")
 
     expect_s3_class(dgeObj_sva, "DGEobj")
 
@@ -10,6 +10,6 @@ test_that("runSVA.R: runSVA()", {
                  regexp = "dgeObj must be specified, be of class 'DGEobj', and should have a 'design' attribute.")
     expect_error(runSVA(dgeObj = t_obj1),
                  regexp = "designMatrixName must be specified, should be of class 'character', and must exist as an attribute on the dgeObj.")
-    expect_error(runSVA(dgeObj = t_obj1, designMatrixName = "ReplicateGroup", method =  "xyz"),
+    expect_error(runSVA(dgeObj = t_obj1, designMatrixName = "ReplicateGroupDesign", method =  "xyz"),
                  regexp = "method must be one of 'leek' or 'be'.")
 })
